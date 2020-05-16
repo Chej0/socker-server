42ac6a52f4370246c8e16295b39c0e24923153af
Sergio <sergiomendez0705@gmail.com>
2020-05-16 16:13:47 -0600

    Seccion 4

---
+++ sockets/sockets.ts
@@ -0,0 +1,16 @@
+import { Socket } from "socket.io";
+import socketIO from 'socket.io';
+
+export const desconectar = (cliente: Socket) => {
+    cliente.on('disconnect', () => {
+        console.log('Cliente desconectado')
+    });
+}
+
+// escuchar mensajes
+export const mensaje = (cliente: Socket, io: socketIO.Server) => {
+    cliente.on('mensaje', (payload: { de:string, cuerpo:string }) => {
+        console.log('MENSAJE RECIBIDO', payload);
+        io.emit('mensaje-nuevo', payload);
+    });
+}
